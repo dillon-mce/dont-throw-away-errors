87f172ce2312f74bd94e2ca0f390c538b92d2e5c
Dillon McElhinney <dillon.mce@gmail.com>
2022-08-22 08:44:29 -0600

    Initial Commit

---
+++ Decoding/DecodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodingApp.swift
+//  Decoding
+//
+//  Created by Dillon McElhinney on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct DecodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
